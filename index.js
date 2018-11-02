84bffbb4ba6360be795cfcd05496250fab2147a2
Patrick Wu <wotingwu@live.com>
2018-11-02 20:51:27 +0800

    Better interface

---
+++ index.js
@@ -0,0 +1,26 @@
+function checkTime(i) {
+    return (i < 10) ? "0" + i : i;
+}
+
+function startTime() {
+    var today = new Date(),
+        h = checkTime(today.getHours()),
+        m = checkTime(today.getMinutes()),
+        s = checkTime(today.getSeconds());
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+
+function unix_prompt() {
+    var unix_home = document.getElementsByClassName('unix_path');
+    for (i = 0; i < unix_home.length; i++) {
+        unix_home[i].innerHTML = '<span class="blue">you</span>@<span class="green">wslu_site</span> <span class="red">$</span>';
+    }
+}
+
+function site_init() {
+    startTime();
+    unix_prompt();
+}
