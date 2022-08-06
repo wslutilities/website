39d53af08bf1b4265ec3778f26432545b6f21575
Jinming Wu, Patrick <me@patrickwu.space>
2022-08-06 12:43:48 +0000

    wslu Wiki update at Sat Aug  6 12:43:48 UTC 2022

---
+++ wslu/assets/contrib.html.fdb9ae30.js
@@ -0,0 +1,1 @@
+const e={key:"v-18f6de66",path:"/legacy/contrib.html",title:"",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"Development",slug:"development",children:[]}],git:{updatedTime:16597898e5,contributors:[{name:"Patrick Wu",email:"me@patrickwu.space",commits:1}]},filePathRelative:"legacy/contrib.md"};export{e as data};
